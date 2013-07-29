55fb0c1af65b581300e56821df2653f8d60f687e
David Fooks <davidfooks@turbulenz.biz>
2013-07-30 00:46:46 +0100

    Added a dynamic tower to knock down

--- scripts/defaultmap.js
+++ scripts/defaultmap.js
@@ -16,8 +16,10 @@ DefaultMap.create = function defaultMapCreateFn(physicsDevice, mathDevice, physi
 
     var count = 0;
 
-    var addBox = function addBoxFn(halfExtents, x, y, z)
+    var addBox = function addBoxFn(halfExtents, x, y, z, dynamic)
     {
+        dynamic = dynamic || false;
+
         var collisionMargin = 0.005;
 
         var boxShape = physicsDevice.createBoxShape({
@@ -29,21 +31,37 @@ DefaultMap.create = function defaultMapCreateFn(physicsDevice, mathDevice, physi
         inertia = mathDevice.v3ScalarMul(inertia, 1.0);
 
         // Initial box is created as a rigid body
-        var boxBody = physicsDevice.createCollisionObject({
-            shape : boxShape,
-            transform : mathDevice.m43BuildTranslation(x, y, z),
-            friction : 0.5,
-            restitution : 0.3,
-            kinematic : false,
-            group: physicsDevice.FILTER_STATIC,
-            mask: physicsDevice.FILTER_ALL
-        });
+        if (dynamic)
+        {
+            var boxBody = physicsDevice.createRigidBody({
+                shape : boxShape,
+                mass: 1.0,
+                inertia: inertia,
+                transform : mathDevice.m43BuildTranslation(x, y, z),
+                friction : 0.5,
+                restitution : 0.3,
+                frozen : false,
+                active : false
+            });
+        }
+        else
+        {
+            var boxBody = physicsDevice.createCollisionObject({
+                shape : boxShape,
+                transform : mathDevice.m43BuildTranslation(x, y, z),
+                friction : 0.5,
+                restitution : 0.3,
+                kinematic : false,
+                group: physicsDevice.FILTER_STATIC,
+                mask: physicsDevice.FILTER_ALL
+            });
+        }
 
-        var position = mathDevice.m43BuildIdentity();
+        var position = mathDevice.m43BuildTranslation(x, y, z);
         var boxSceneNode = SceneNode.create({
                 name: 'BoxPhys' + count,
                 local: position,
-                dynamic: false,
+                dynamic: dynamic,
                 disabled: false
             });
 
@@ -52,17 +70,25 @@ DefaultMap.create = function defaultMapCreateFn(physicsDevice, mathDevice, physi
         var physicsNode = {
             body : boxBody,
             target : boxSceneNode,
-            dynamic : false
+            dynamic : dynamic
         };
 
         scene.addRootNode(boxSceneNode);
         boxSceneNode.physicsNodes = [physicsNode];
-        // TODO: should be setStatic?  setDynamic takes no args.
-        //boxSceneNode.setDynamic(false);
-        boxSceneNode.setDynamic();
 
         physicsManager.physicsNodes.push(physicsNode);
-        physicsManager.enableHierarchy(boxSceneNode, true);
+        if (dynamic)
+        {
+            dynamicsWorld.addRigidBody(boxBody);
+            physicsManager.dynamicPhysicsNodes.push(physicsNode);
+        }
+        else
+        {
+            // TODO: should be setStatic?  setDynamic takes no args.
+            //boxSceneNode.setDynamic(false);
+            boxSceneNode.setDynamic();
+            physicsManager.enableHierarchy(boxSceneNode, true);
+        }
     };
 
     // landing pad
@@ -79,6 +105,13 @@ DefaultMap.create = function defaultMapCreateFn(physicsDevice, mathDevice, physi
     addBox([4.0, 1.0, 4.0], 0.0,   21.0,  35.0);
     addBox([4.0, 1.0, 4.0], -35.0, 21.0,   0.0);
     addBox([4.0, 1.0, 4.0], 0.0,   21.0, -35.0);
+
+    // topple
+    /*addBox([0.5, 0.5, 0.5], 35.0, 22.5, 0.0, true);
+    addBox([0.5, 0.5, 0.5], 35.0, 23.5, 0.0, true);
+    addBox([0.5, 0.5, 0.5], 35.0, 24.5, 0.0, true);
+    addBox([0.5, 0.5, 0.5], 35.0, 25.5, 0.0, true);
+    addBox([0.5, 0.5, 0.5], 35.0, 26.5, 0.0, true);*/
 
     // arch pillars
     addBox([2.0, 10.0, 2.0], 100.0, 10.0, -35.0);
@@ -97,6 +130,11 @@ DefaultMap.create = function defaultMapCreateFn(physicsDevice, mathDevice, physi
     addBox([2.0, 3.0, 2.0], 200.0, 3.0, 10.0);
     // arch bar
     addBox([1.0, 1.0, 10.0],  200.0, 7.0, 0.0);
+
+    // arch end pillars
+    addBox([2.0, 10.0, 2.0], 250.0,  10.0,   0.0);
+    // arch end pillar landing pad
+    addBox([4.0, 1.0, 4.0], 250.0,  21.0,   0.0);
 
     return defaultMap;
 };
