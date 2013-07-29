f757e7f35fcc6c1e960e34b9a942b56fe5ff488e
David Fooks <davidfooks@turbulenz.biz>
2013-07-29 23:55:13 +0100

    Assualt course map

--- scripts/defaultmap.js
+++ scripts/defaultmap.js
@@ -8,57 +8,95 @@ function DefaultMap() {}
 
 DefaultMap.prototype =
 {
-
 };
 
 DefaultMap.create = function defaultMapCreateFn(physicsDevice, mathDevice, physicsManager, dynamicsWorld, scene)
 {
     var defaultMap = new DefaultMap();
 
-    var collisionMargin = 0.005;
+    var count = 0;
 
-    var boxShape = physicsDevice.createBoxShape({
-            halfExtents : [10.0, 1.0, 10.0],
-            margin : collisionMargin
+    var addBox = function addBoxFn(halfExtents, x, y, z)
+    {
+        var collisionMargin = 0.005;
+
+        var boxShape = physicsDevice.createBoxShape({
+                halfExtents : halfExtents,
+                margin : collisionMargin
+            });
+
+        var inertia = mathDevice.v3Copy(boxShape.inertia);
+        inertia = mathDevice.v3ScalarMul(inertia, 1.0);
+
+        // Initial box is created as a rigid body
+        var boxBody = physicsDevice.createCollisionObject({
+            shape : boxShape,
+            transform : mathDevice.m43BuildTranslation(x, y, z),
+            friction : 0.5,
+            restitution : 0.3,
+            kinematic : false,
+            group: physicsDevice.FILTER_STATIC,
+            mask: physicsDevice.FILTER_ALL
         });
 
-    var inertia = mathDevice.v3Copy(boxShape.inertia);
-    inertia = mathDevice.v3ScalarMul(inertia, 1.0);
+        var position = mathDevice.m43BuildIdentity();
+        var boxSceneNode = SceneNode.create({
+                name: 'BoxPhys' + count,
+                local: position,
+                dynamic: false,
+                disabled: false
+            });
 
-    // Initial box is created as a rigid body
-    var boxBody = physicsDevice.createCollisionObject({
-        shape : boxShape,
-        mass : 20.0,
-        transform : mathDevice.m43BuildTranslation(0.0, 1.0, 0.0),
-        friction : 0.5,
-        restitution : 0.3,
-        kinematic : false,
-        group: physicsDevice.FILTER_STATIC,
-        mask: physicsDevice.FILTER_ALL
-    });
+        count += 1;
 
-    var position = mathDevice.m43BuildTranslation(0.0, 5.0, 0.0);
-    var boxSceneNode = SceneNode.create({
-            name: 'BoxPhys',
-            local: position,
-            dynamic: false,
-            disabled: false
-        });
+        var physicsNode = {
+            body : boxBody,
+            target : boxSceneNode,
+            dynamic : false
+        };
 
-    var physicsNode = {
-        body : boxBody,
-        target : boxSceneNode,
-        dynamic : false
+        scene.addRootNode(boxSceneNode);
+        boxSceneNode.physicsNodes = [physicsNode];
+        // TODO: should be setStatic?  setDynamic takes no args.
+        //boxSceneNode.setDynamic(false);
+        boxSceneNode.setDynamic();
+
+        physicsManager.physicsNodes.push(physicsNode);
+        physicsManager.enableHierarchy(boxSceneNode, true);
     };
 
-    scene.addRootNode(boxSceneNode);
-    boxSceneNode.physicsNodes = [physicsNode];
-    // TODO: should be setStatic?  setDynamic takes no args.
-    //boxSceneNode.setDynamic(false);
-    boxSceneNode.setDynamic();
+    // landing pad
+    addBox([10.0, 1.0, 10.0], 0.0, 1.0, 0.0);
 
-    physicsManager.physicsNodes.push(physicsNode);
-    physicsManager.enableHierarchy(boxSceneNode, true);
+    // pillars
+    addBox([2.0, 10.0, 2.0], 35.0,  10.0,   0.0);
+    addBox([2.0, 10.0, 2.0], 0.0,   10.0,  35.0);
+    addBox([2.0, 10.0, 2.0], -35.0, 10.0,   0.0);
+    addBox([2.0, 10.0, 2.0], 0.0,   10.0, -35.0);
+
+    // pillars landing pads
+    addBox([4.0, 1.0, 4.0], 35.0,  21.0,   0.0);
+    addBox([4.0, 1.0, 4.0], 0.0,   21.0,  35.0);
+    addBox([4.0, 1.0, 4.0], -35.0, 21.0,   0.0);
+    addBox([4.0, 1.0, 4.0], 0.0,   21.0, -35.0);
+
+    // arch pillars
+    addBox([2.0, 10.0, 2.0], 100.0, 10.0, -35.0);
+    addBox([2.0, 10.0, 2.0], 100.0, 10.0, 35.0);
+    // arch bar
+    addBox([1.0, 1.0, 35.0],  100.0, 21.0, 0.0);
+
+    // arch pillars
+    addBox([2.0, 7.0, 2.0], 150.0, 7.0, -20.0);
+    addBox([2.0, 7.0, 2.0], 150.0, 7.0, 20.0);
+    // arch bar
+    addBox([1.0, 1.0, 20.0],  150.0, 15.0, 0.0);
+
+    // arch pillars
+    addBox([2.0, 3.0, 2.0], 200.0, 3.0, -10.0);
+    addBox([2.0, 3.0, 2.0], 200.0, 3.0, 10.0);
+    // arch bar
+    addBox([1.0, 1.0, 10.0],  200.0, 7.0, 0.0);
 
     return defaultMap;
 };
